eb16ebf96925d9dcbc08b7186b276e30f485e15d
1Mathias <mathiasabdisa@gmail.com>
2022-01-27 17:42:58 +0300

    server editted

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
+//Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
-app.use(express.static(__dirname + '/dist/Task-Manager'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/Task-Manager/index.html'));});
+// Server only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/frontend'));
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname +
+        '/dist/frontend/index.html'));
+});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/frontend/'}),
+);
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
