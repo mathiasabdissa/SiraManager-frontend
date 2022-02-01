8ef143767cf35f87349e7ecae3805125fc1d2d7d
1Mathias <mathiasabdisa@gmail.com>
2022-02-01 15:41:59 +0300

    Bug fixed

--- src/app/pages/new-list/new-list.component.ts
+++ src/app/pages/new-list/new-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { TaskService } from 'src/app/services/task.service';
 import { HttpClient } from '@angular/common/http'
+import { Router } from '@angular/router';
+import { List } from 'src/app/models/list.model';
 
 @Component({
   selector: 'app-new-list',
@@ -9,15 +11,16 @@ import { HttpClient } from '@angular/common/http'
 })
 export class NewListComponent implements OnInit {
 
-  constructor(private taskService:TaskService) { }
+  constructor(private taskService:TaskService,private router: Router) { }
 
   ngOnInit(): void {
   }
   
 
   createList(title:string){
-    this.taskService.createList(title).subscribe((response:any)=>{
-      console.log(response);
+    this.taskService.createList(title).subscribe((list)=>{
+      // Now we navigate to /lists/task._id
+      this.router.navigate([ '/lists']); 
     });
   }
 
