554d34342fc2b1f9f98e0cc840689445d50897a1
1Mathias <mathiasabdisa@gmail.com>
2022-08-04 08:30:34 +0300

    task view updates

--- src/app/pages/task-view/task-view.component.ts
+++ src/app/pages/task-view/task-view.component.ts
@@ -15,12 +15,16 @@ export class TaskViewComponent implements OnInit {
   lists: List[];
   tasks: Task[];
 
+  selectedListId:string;
+
   constructor(private taskService: TaskService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(
       (params: Params) => {
-        console.log(params);
+        if(params.listId){
+          this.selectedListId=params.listId
+        }
         this.taskService.getTasks(params.listId).subscribe((tasks: any) => {
           this.tasks = tasks;
         })
@@ -30,11 +34,26 @@ export class TaskViewComponent implements OnInit {
       this.lists = lists;
     })
   }
+
   onTaskClick(task: Task){
     // to make the task set to completed
     this.taskService.complete(task).subscribe(()=>{
       console.log("Completed Successfully");
       task.completed=!task.completed;
+    })
+  }
+
+  onDeleteListClick() {
+    this.taskService.deleteList(this.selectedListId).subscribe((res: any) => {
+      this.router.navigate(['/lists']);
+      console.log(res);
+    })
+  }
+  
+  onDeleteTaskClick(id: string) {
+    this.taskService.deleteTask(this.selectedListId, id).subscribe((res: any) => {
+      this.tasks = this.tasks.filter(val => val._id !== id);
+      console.log(res);
     })
   }
 }
